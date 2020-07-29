5672093c3e8b06c38d7a96b90e38c9df61afe8a4
Abhay Painuly <65288448+abhaypainuly@users.noreply.github.com>
2020-07-29 18:05:51 +0530

    Delete insertion_sort.py

--- searching/insertion_sort.py
+++
@@ -1,9 +0,0 @@
-def insertion_sort(unsorted):
-    for i in range(1,len(unsorted)):
-        j = i-1
-        temp = unsorted[i]
-        while j>=0 and unsorted[j]>temp:
-            unsorted[j+1] = unsorted[j]
-            j -= 1
-        unsorted[j+1] = temp
-    return unsorted
